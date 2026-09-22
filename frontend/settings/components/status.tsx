64f9eb2b6cf4c2b71556abaf2d2e36b483996292
Zaher Ghaibeh <zaher@zaher.dev>
2026-09-22 21:41:07 +0300

    cleanup

--- frontend/settings/components/status.tsx
+++ frontend/settings/components/status.tsx
@@ -17,56 +17,47 @@ const fileSystemLable = filesystem_writable
       : __( 'File System Not writeable', 'redis-cache' );
 
 export const Status = () => (
-    <Card.Root>
-        <Card.Content>
-            <div className="flex items-center justify-between gap-x-6">
-                <div className="justify-start">
-                    <Icon icon={ CheckFilled } size={ 48 } className="green" />
+    <Card.Root className="status-card">
+        <Card.Content className="status-card__content">
+            <div className="justify-start">
+                <Icon icon={ CheckFilled } size={ 48 } className="green" />
+            </div>
+            <div className="status-card__status">
+                <div className="flex flex-col">
+                    <span className="font-xl">
+                        { createInterpolateElement(
+                            __( '<strong>Redis Object Cache is active</strong>', 'redis-cache' ),
+                            { strong: <strong /> },
+                        ) }
+                    </span>
                 </div>
-                <div className="flex flex-col grow gap-y-6">
-                    <div className="flex flex-col">
-                        <span className="font-xl">
-                            { createInterpolateElement(
-                                __( '<strong>Redis Object Cache is active</strong>', 'redis-cache' ),
-                                { strong: <strong /> },
-                            ) }
-                        </span>
-                        <span className="gray font-lg mt-3">
-                            { __( 'WordPress is connected to Redis', 'redis-cache' ) }.
-                        </span>
+                <div className="status-card__features-stats">
+                    <div className={ typeof status === 'string' || status === null ? 'error-circle' : 'active-circle' }>
+                        { sprintf( __( 'Redis %s', 'redis-cache' ), get_status ) }
                     </div>
-                    <div className="flex gap-x-4 gap-y-8 font-md gray">
-                        <div
-                            className={
-                                typeof status === 'string' || status === null ? 'error-circle' : 'active-circle'
-                            }
-                        >
-                            { sprintf( __( 'Redis %s', 'redis-cache' ), get_status ) }
-                        </div>
-                        <div className="active-circle">{ __( 'Drop-in valid', 'redis-cache' ) }</div>
-                        <div className={ fileSystemStatus !== 'writeable' ? 'error-circle' : 'active-circle' }>
-                            { fileSystemLable }
-                        </div>
+                    <div className="active-circle">{ __( 'Drop-in valid', 'redis-cache' ) }</div>
+                    <div className={ fileSystemStatus !== 'writeable' ? 'error-circle' : 'active-circle' }>
+                        { fileSystemLable }
                     </div>
                 </div>
-                <div className="flex justify-end gap-x-2">
-                    <Button
-                        title={ __( 'Flush Cache', 'redis-cache' ) }
-                        variant="primary"
-                        role="button"
-                        className="button-large"
-                    >
-                        { __( 'Flush Cache', 'redis-cache' ) }
-                    </Button>
-                    <Button
-                        title={ __( 'Disable Object Cache', 'redis-cache' ) }
-                        variant="secondary"
-                        role="button"
-                        className="button-large"
-                    >
-                        { __( 'Disable Object Cache', 'redis-cache' ) }
-                    </Button>
-                </div>
+            </div>
+            <div className="status-card__action-buttons">
+                <Button
+                    title={ __( 'Flush Cache', 'redis-cache' ) }
+                    variant="primary"
+                    role="button"
+                    className="button-large"
+                >
+                    { __( 'Flush Cache', 'redis-cache' ) }
+                </Button>
+                <Button
+                    title={ __( 'Disable Object Cache', 'redis-cache' ) }
+                    variant="secondary"
+                    role="button"
+                    className="button-large"
+                >
+                    { __( 'Disable Object Cache', 'redis-cache' ) }
+                </Button>
             </div>
         </Card.Content>
     </Card.Root>
